7b36d637f509cf56788765645129ed6e11df9d2b
guofeng <181151038@qq.com>
2017-04-02 17:28:26 +0800

    Initial commit

---
+++ src/com/youweek/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.youweek.demo;
+
+import sun.applet.Main;
+
+/**
+ * Created by Administrator on 2017/4/2.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello-worlds");
+    }
+}
